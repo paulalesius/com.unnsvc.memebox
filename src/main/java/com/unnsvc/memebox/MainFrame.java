69246306b0c8f042c64123172d67581738e88a5d
Paul Alesius <paul@unnservice.com>
2017-06-18 23:55:56 +0200

    Force look and feels if installed

--- src/main/java/com/unnsvc/memebox/MainFrame.java
+++ src/main/java/com/unnsvc/memebox/MainFrame.java
@@ -1,7 +1,13 @@
 
 package com.unnsvc.memebox;
 
+import java.awt.Dimension;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
 
 public class MainFrame extends JFrame {
 
@@ -13,6 +19,38 @@ public class MainFrame extends JFrame {
 
 	public static void main(String... args) throws Exception {
 
-		MainFrame frame = new MainFrame();
+		MemeboxUtils.configureLookAndFeel();
+
+		SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+
+				MainFrame frame = new MainFrame();
+				frame.setSize(new Dimension(1024, 768));
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.configureTabs();
+				frame.setVisible(true);
+			}
+		});
+	}
+
+	private void configureTabs() {
+
+		JTabbedPane tabbedPane = new JTabbedPane();
+		JComponent mainTab = createMainTab();
+		tabbedPane.addTab("Memebox", mainTab);
+		JComponent optionsTab = createOptionsTab();
+		tabbedPane.addTab("Options", optionsTab);
+		add(tabbedPane);
+	}
+
+	private JComponent createOptionsTab() {
+
+		return new JPanel();
+	}
+
+	private JComponent createMainTab() {
+
+		return new JPanel();
 	}
 }
--- src/main/java/com/unnsvc/memebox/MemeboxUtils.java
+++ src/main/java/com/unnsvc/memebox/MemeboxUtils.java
@@ -1,11 +1,15 @@
 
 package com.unnsvc.memebox;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class MemeboxUtils {
+
+	private static Logger log = LoggerFactory.getLogger(MemeboxUtils.class);
 
 	public static void listChildren(Document document, MemeboxListChildrenCallback callback) {
 
@@ -33,5 +37,44 @@ public class MemeboxUtils {
 	public interface MemeboxListChildrenCallback {
 
 		public void onNode(Node node);
+	}
+
+	/**
+	 * @TODO options should allow manual configuration of this, saved in
+	 *       memebox.xml
+	 * @throws Exception
+	 */
+	public static void configureLookAndFeel() throws Exception {
+
+		for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+
+			if ("com.sun.java.swing.plaf.gtk.GTKLookAndFeel".equals(info.getClassName())) {
+
+				javax.swing.UIManager.setLookAndFeel(info.getClassName());
+				break;
+			} else if ("com.sun.java.swing.plaf.windows.WindowsLookAndFeel".equals(info.getClassName())) {
+
+				javax.swing.UIManager.setLookAndFeel(info.getClassName());
+				break;
+			}
+
+			log.debug("Available look and feel: " + info.getClassName());
+		}
+
+		// try {
+		//
+		// System.err.println("System look and feel is " +
+		// UIManager.getSystemLookAndFeelClassName());
+		// UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		// } catch (UnsupportedLookAndFeelException e1) {
+		// try {
+		// System.err.println("Configuring crossplatform");
+		// UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		// } catch (UnsupportedLookAndFeelException e2) {
+		//
+		// throw new Exception("Failed to set Look and Feel, this is a bug,
+		// please report.");
+		// }
+		// }
 	}
 }
